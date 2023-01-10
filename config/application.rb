a8d71d0048b1088a212f838b63d508d6f65d4820
k_akihiro1 <k.shuhari@gmail.com>
2023-01-10 22:31:52 +0900

    sample_articleモデルとテーブルの作成

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,13 @@ module RailsPostApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.generators do |g|
+      g.jbuilder false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework false
+    end
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
