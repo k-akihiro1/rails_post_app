92683eb1723a0c8c85573d3e43339847e0510d6c
k_akihiro1 <k.shuhari@gmail.com>
2023-01-22 19:25:35 +0900

    articleの編集・更新の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,20 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+    # PATCH/PUT /sample_articles/1 or /sample_articles/1.json
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_url(@article), notice: "article was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :content)
