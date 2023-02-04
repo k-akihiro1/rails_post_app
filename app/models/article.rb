d3b4448567fc269f128271d43954ea365a3e2bbe
k_akihiro1 <k.shuhari@gmail.com>
2023-02-05 04:52:16 +0900

    タグと記事の関係性を修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
   validates :title, :content, presence: true
   belongs_to :user
   has_many :tag_articles
-  has_many :tag_articles, through: :tag_articles
+  has_many :tags, through: :tag_articles
 end
