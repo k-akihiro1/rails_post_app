a65d4ac633c7241b9b8d29fd8195de47e40bc1a6
k_akihiro1 <k.shuhari@gmail.com>
2023-02-04 14:58:44 +0900

    ページネーションの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :destroy]
   def index
-    @articles = Article.all
+    @articles = Article.all.page(params[:page]).per(10)
   end
 
   def show
